7859a8e9dad52da717c3235f2694030edc55591a
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 00:37:03 +0500

    File of Question 2

---
+++ Question2.js
@@ -0,0 +1,4 @@
+"use strict";
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python toda
+let personName = "Huma";
+console.log(`Hello , ${personName} would you like to learn some typescript today?`);
---
+++ Question2.ts
@@ -0,0 +1,5 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python toda
+
+let personName:string ="Huma";
+
+console.log(`Hello , ${personName} would you like to learn some typescript today?`) 
