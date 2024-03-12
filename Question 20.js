acd21f0e9bcabecdee95c1723778d7820ca36d1b
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:26:06 +0500

    File of Question 20

---
+++ Question 20.js
@@ -0,0 +1,7 @@
+"use strict";
+// Makig Array of countries
+let countries = ["LONDON", "JAPAN", "PAKISTAN", "AMERICA", "BRAZIL"];
+// print the message of list
+console.log("list of countries");
+// print the values of Array in the form of list
+countries.map((item) => console.log(item));
---
+++ Question 20.ts
@@ -0,0 +1,6 @@
+// Makig Array of countries
+let countries:string[]=["LONDON","JAPAN","PAKISTAN","AMERICA","BRAZIL"];
+// print the message of list
+console.log("list of countries");
+// print the values of Array in the form of list
+countries.map((item)=>console.log(item));
