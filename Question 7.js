0b6283ecaf3f8a6e6e65d4f3668aa91c69a24eab
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:10:44 +0500

    File of Question 7

---
+++ Question 7.js
@@ -0,0 +1,13 @@
+"use strict";
+//addition
+let addition = 6 + 2;
+console.log("Addition", addition);
+//subtraction
+let sub = 10 - 2;
+console.log("subtraction", sub);
+//multiplication
+let multiplication = 4 * 2;
+console.log("multiplication", multiplication);
+//division
+let division = 16 / 2;
+console.log("division", division);
---
+++ Question 7.ts
@@ -0,0 +1,20 @@
+
+//addition
+let addition:number = 6+2;
+
+console.log("Addition",addition);
+
+//subtraction
+let sub:number = 10-2;
+
+console.log("subtraction",sub)
+
+//multiplication
+
+let multiplication:number = 4 * 2;
+console.log("multiplication",multiplication);
+
+//division
+
+let division:number = 16/2;
+console.log("division",division);
