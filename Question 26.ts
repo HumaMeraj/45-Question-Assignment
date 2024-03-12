b780bf58cb8ace03255685f25f1df228da0292e7
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:33:08 +0500

    File of Question 26

---
+++ Question 26.ts
@@ -0,0 +1,16 @@
+// Define variable
+let aliencolor ="green";
+//First version
+if(aliencolor ==="green"){
+//console.log("player just earned 5 points for shooting the alien.");
+}
+else{
+    //console.log("player just earned 10 points");
+}
+// second version
+if(aliencolor === "blue"){
+    console.log("I am come from If statment.");
+}
+else{
+    console.log("I am come from Else statment.");
+}
