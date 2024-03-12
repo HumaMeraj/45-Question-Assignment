1ee5f13fca2e10c46ab95b9c220043eda3d1a38e
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:16:55 +0500

    File of Question 12

---
+++ Question 12.js
@@ -0,0 +1,8 @@
+"use strict";
+// make array
+let names = ["Huma", "Amber", "Uzma"];
+// send message
+let message = "would you like to learn type script";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
---
+++ Question 12.ts
@@ -0,0 +1,8 @@
+// make array
+let names :string [] =["Huma","Amber","Uzma"];
+
+// send message
+let message:string ="would you like to learn type script"
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
