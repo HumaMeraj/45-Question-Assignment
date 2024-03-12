76a781524815351531dd177dee7ba8a6acfbdfc3
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:16:12 +0500

    File of Question 11

---
+++ Question 11.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ["Huma", "Amber", "Uzma"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
---
+++ Question 11.ts
@@ -0,0 +1,6 @@
+
+let names :string [] =["Huma","Amber","Uzma"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+
