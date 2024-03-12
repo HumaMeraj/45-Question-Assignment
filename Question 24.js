b8d5a16f47fba3b9fcd3fe2db7d30610f2fc727d
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:28:57 +0500

    File of Question 24

---
+++ Question 24.js
@@ -0,0 +1,52 @@
+"use strict";
+//Define variables
+let apple = "apple";
+let uppercaseApple = "APPLE";
+let number = 10;
+let twenty = 20;
+let numbers = [1, 2, 3, 4];
+//Test for equality and inequality with strings
+//console.log("Is appple is equal to apple");
+//console.log(apple == "apple");
+//console.log("\nIs apple is not equal to apple?");
+//console.log(apple != "apple");
+//Test using the lower case function
+//console.log("\nIs APPLE is equal to apple after converting to lowercase?");
+//console.log(uppercaseApple.toLowerCase()=="apple");
+//console.log("\nIs APPLE is not equal to apple after converting to lowercase?");
+//console.log(uppercaseApple.toLowerCase()!="apple");
+//Numerical Tests
+// Equal to
+//console.log("\n Is ten is equal to twenty?");
+//console.log(ten == twenty);
+// Not Equal to
+//console.log("\n Is ten is not equal to twenty?");
+//console.log(ten != twenty);
+// Greater than
+//console.log("\n Is ten is greater than zero?");
+//console.log(ten > 0);
+// Less than
+//console.log("\n Is twenty is less than 10?");
+//console.log(twenty < 10);
+//Greater than or equal to
+//console.log("\n Is ten is greater than or equal to 5?");
+//console.log(ten >= 5);
+//Less than or equal to
+//console.log("\n Is twenty is less than or equal to 10?");
+//console.log(twenty <= 10);
+//Test using "and" and "or" operators
+//console.log("\n twenty is not equal to 10 and twenty is greater than 10");
+//console.log(twenty != 10 && twenty > 10);
+//using && (and) operator
+//console.log("\n twenty is not equal to 10 and twenty is greater than 10");
+//console.log(twenty != 10 && twenty > 30);
+//using ||(OR)operator
+//console.log("\n 20 is greater than 50 OR 20 is equal to 20");
+//console.log("\n 20 is greater than 50 OR 20 is not equal to 20");
+//console.log(20 > 50 || 20 != 20);
+// Test whether an item is in a Array(True)
+//onsole.log("Is 3 in numbers?I predict True.");
+//console.log(3 in numbers);
+// Test whether an item is not in a Array(False)
+console.log("Is 5 not in numbers?I predict true.");
+console.log(5 in numbers);
