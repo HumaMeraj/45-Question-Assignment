ee53e4bc73a932f432642492ff10288c0781eb00
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 00:41:08 +0500

    File of Question 3

---
+++ Question3.js
@@ -0,0 +1,8 @@
+"use strict";
+let personName = "aqsa";
+// lower case
+console.log(personName.toLowerCase());
+// upper case
+console.log(personName.toUpperCase());
+// title case
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1, 4));
---
+++ Question3.ts
@@ -0,0 +1,12 @@
+let personName:string ="aqsa";
+
+// lower case
+console.log(personName.toLowerCase());
+
+// upper case
+
+console.log(personName.toUpperCase());
+
+// title case
+
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1,4));
