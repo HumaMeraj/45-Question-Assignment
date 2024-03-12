c3e471806907e411338748f03057a30d2ea9d246
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:08:38 +0500

    File of Question 5

---
+++ Question 5.js
@@ -0,0 +1,6 @@
+"use strict";
+let famouspersons = "Albert Einstine";
+let famousquote = '"A person who never made a mistake never tried anything new"';
+//compose message
+let message = `${famouspersons}once said , ${famousquote}`;
+console.log(message);
---
+++ Question 5.ts
@@ -0,0 +1,10 @@
+
+
+let famouspersons:string="Albert Einstine";
+let famousquote: string ='"A person who never made a mistake never tried anything new"';
+
+//compose message
+
+let message:string=`${famouspersons}once said , ${famousquote}`
+
+console.log(message);
