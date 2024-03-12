4be6955ed37b19fae728436d3aa62a4f08460e9d
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:12:28 +0500

    File of Question 8

---
+++ Question 8.js
@@ -0,0 +1,9 @@
+"use strict";
+// addition
+console.log(6 + 2);
+//subtraction
+console.log(10 - 2);
+//multiplication
+console.log(4 * 2);
+//division
+console.log(16 / 2);
---
+++ Question 8.ts
@@ -0,0 +1,11 @@
+// addition
+console.log(6+2);
+
+//subtraction
+console.log(10-2);
+
+//multiplication
+console.log(4*2);
+
+//division
+console.log(16/2);
