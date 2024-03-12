5f9519ad09d577eb1bc088106217a075d9c50319
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:29:44 +0500

    File of Question 25

---
+++ Question 25.ts
@@ -0,0 +1,10 @@
+//Define variable
+let alienColor ="green";
+if(alienColor ==="green"){
+    //code Block
+    console.log("Alien color is green Player just earned 5 points");
+}
+
+if(alienColor === "white"){
+    console.log("Alien color is white");
+}
