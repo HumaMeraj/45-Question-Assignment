03c937c81a1c4b84c3961879ae2112d31594077d
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:33:46 +0500

    File of Question 27

---
+++ Question 27.ts
@@ -0,0 +1,34 @@
+// define varaible
+let aliencolor="green";
+// using If and Else-if statments
+if(aliencolor ==="green"){
+    console.log("(version 1) you shot down green alien you have earned 5 points");
+}
+else if (aliencolor === "yellow"){
+console.log("you shot down yellow alien you have earned 10 points");
+}
+else if (aliencolor ==="red"){
+    console.log("you shot down red alien you have earned 15 points");
+}
+// version 2
+let aliencolor2 = "yellow";
+if (aliencolor2 === "green"){
+    console.log("( you shot down green alien you have earned 5 points");
+}
+else if (aliencolor2 === "yellow"){
+    console.log("(version 2) you shot down yellow alien you have earned 10 points");
+}
+else if (aliencolor2 ==="red"){
+ console.log("you shot down red alien you have earned 15 points");
+}
+// version 3
+let aliencolor3 = "red";
+if (aliencolor3 === "green"){
+    console.log("( you shot down green alien you have earned 5 points");
+}
+else if (aliencolor3 === "yellow"){
+    console.log("(version 2) you shot down yellow alien you have earned 10 points");
+}
+else if (aliencolor3 ==="red"){
+    console.log("(version 3) you shot down red alien you have earned 15 points");
+   }
