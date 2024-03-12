e7f7bb84ae6f7a5515ac256556ea4f2fbca0d0d6
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:27:33 +0500

    File of Question 22

---
+++ Question 22.js
@@ -0,0 +1,7 @@
+"use strict";
+//Creating a Array
+let errorArray = ["A", "B", "C", "D"];
+//Producing error! by Accessing invalid index of Array
+//console.log(errorArray[10]);
+//Error Removed
+console.log(errorArray[1]);
---
+++ Question 22.ts
@@ -0,0 +1,8 @@
+//Creating a Array
+let errorArray:string[]=["A","B","C","D"];
+
+//Producing error! by Accessing invalid index of Array
+//console.log(errorArray[10]);
+
+//Error Removed
+console.log(errorArray[1]);
