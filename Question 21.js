2e27bfbc5a0352b7459734ef56831972a9c851ea
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:26:52 +0500

    File of Question 21

---
+++ Question 21.js
@@ -0,0 +1,7 @@
+"use strict";
+let ITCourse = {
+    CourseName: "TypeScript and JavaScript",
+    Location: "GovernorHouse Sindh",
+    OnsiteStudent: 50000
+};
+console.log(ITCourse);
---
+++ Question 21.ts
@@ -0,0 +1,12 @@
+// create object
+interface ITCourse{
+    CourseName:string ;
+    Location:string;
+    OnsiteStudents:number;
+}
+let ITCourse={
+    CourseName: "TypeScript and JavaScript",
+    Location:"GovernorHouse Sindh",
+    OnsiteStudent:50000
+};
+console.log(ITCourse);
