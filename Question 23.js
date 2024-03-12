fb80851a526cd80f3e1e949bce8f711dbbf92505
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:28:19 +0500

    File of Question 23

---
+++ Question 23.js
@@ -0,0 +1,34 @@
+"use strict";
+// make variable
+let five = 5;
+let ten = 10;
+//Test 1
+console.log("Test 1:five is equal to 5?");
+console.log(five == 5);
+//Test 2
+console.log("\nTest 2:ten is equal to 10");
+console.log(ten == 10);
+//Test 3
+console.log("\nTest 3:5 is not equal to 10");
+console.log(five != ten);
+// Test 4
+console.log("\nTest 4:10 is greater than 5");
+console.log(ten > 5);
+// Test 5
+console.log("\n Test 5:5 is smaller than 10");
+console.log(five < ten);
+// Test 6
+console.log("\nTest 6:10 is smaller than 5");
+console.log(10 < 5);
+//Test 7
+console.log("\nTest 7:5 is equal to 10");
+console.log(5 == ten);
+//Test 8
+console.log("\nTest 8:10 is not equal to 10");
+console.log(ten != 10);
+//Test 9
+console.log("\nTest 9:5 is greater than 10");
+console.log(five > 10);
+//Test 10
+console.log("\nTest 10:10 is smaller than 5");
+console.log(10 < 5);
