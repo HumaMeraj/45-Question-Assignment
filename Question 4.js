767460f813228e6ab16245303fa8018bf3653104
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:07:30 +0500

    File of Question 4

---
+++ Question 4.js
@@ -0,0 +1,3 @@
+"use strict";
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+console.log('Albert Einstein once said, "A person who never made a mistake never tried anything new".');
---
+++ Question 4.ts
@@ -0,0 +1,3 @@
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log('Albert Einstein once said, "A person who never made a mistake never tried anything new".')
