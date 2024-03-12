e3ded1bf997da4f94ce6291f6fb74d9511504a6b
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:24:45 +0500

    File of Question 19

---
+++ Question 19.js
@@ -0,0 +1,5 @@
+"use strict";
+//Array
+let guestArray = ["Fatima", "Suman", "Hareem"];
+// print message
+console.log(guestArray.length, "people are invited to dinner");
---
+++ Question 19.ts
@@ -0,0 +1,6 @@
+//Array
+let guestArray: string [] = ["Fatima","Suman","Hareem"];
+
+// print message
+
+console.log(guestArray.length,"people are invited to dinner");
