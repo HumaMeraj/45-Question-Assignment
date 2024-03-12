d374a4a45d1bf24d3f3658e221a6d2f4efb136d1
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:18:36 +0500

    File of Question 14

---
+++ Question 14.js
@@ -0,0 +1,5 @@
+"use strict";
+//Array
+let guestArray = ["Fatima", "Suman", "Hareem"];
+//print message
+guestArray.map((items) => console.log(`Hello,${items} you are invited to dinner`));
---
+++ Question14.ts
@@ -0,0 +1,4 @@
+//Array
+let guestArray: string [] = ["Fatima","Suman","Hareem"];
+//print message
+guestArray.map((items) => console.log(`Hello,${items} you are invited to dinner`));
