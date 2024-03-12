19fa086a64fa18257d1344f415ab5fac845aeff0
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:24:01 +0500

    File of Question 18

---
+++ Question 18.js
@@ -0,0 +1,14 @@
+"use strict";
+let placetovisit = ["Hongkong", "America", "Pakistan", "Brazil", "Iran"];
+//console.log(placetovisit);
+//console.log(placetovisit.sort());
+//console.log(placetovisit);
+//console.log(placetovisit.sort().reverse());
+//console.log(placetovisit);
+//console.log(placetovisit.reverse());
+//console.log(placetovisit.reverse());
+let sortedArray = ["Hongkong", "America", "Pakistan", "Brazil", "Iran"];
+sortedArray.sort();
+console.log(sortedArray);
+sortedArray.reverse();
+console.log(sortedArray);
