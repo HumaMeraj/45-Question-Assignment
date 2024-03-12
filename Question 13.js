f7274bee9a942b139533ddf5766cd0b1e89cd6c7
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-13 01:17:45 +0500

    File of Question 13

---
+++ Question 13.js
@@ -0,0 +1,3 @@
+"use strict";
+let transportation = ["Honda Motorcycle", "Honda city", "Honda civic", "BMW"];
+transportation.map((item) => console.log("I would like to own a", item));
---
+++ Question 13.ts
@@ -0,0 +1,5 @@
+let transportation:string[] = ["Honda Motorcycle","Honda city","Honda civic","BMW"];
+
+transportation.map((item) => console.log("I would like to own a",item,));
+
+
