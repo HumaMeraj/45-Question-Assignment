1266a06b60a90d98d8d9d1429ca07f6ca5e9cde8
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-15 01:00:15 +0500

    File of Question 29

---
+++ Question 29.js
@@ -0,0 +1,15 @@
+"use strict";
+//create Array
+let favorite_fruit = [] = ["Mango", "Strawberry", "Orange"];
+if (favorite_fruit.includes("Mango")) {
+    console.log("I really like mangoes");
+}
+if (favorite_fruit.includes("Strawberry")) {
+    console.log("I really like strawberry");
+}
+if (favorite_fruit.includes("Apple")) {
+    console.log("I really like an Apple");
+}
+if (favorite_fruit.includes("Orange")) {
+    console.log("I really like orange");
+}
---
+++ Question 29.ts
@@ -0,0 +1,19 @@
+//create Array
+let favorite_fruit = []= ["Mango","Strawberry","Orange"];
+
+// using five independent If statments
+if(favorite_fruit.includes("Mango")){
+    console.log("I really like mangoes");
+}
+if (favorite_fruit.includes("Strawberry")){
+    console.log("I really like strawberry");
+}
+if (favorite_fruit.includes("Apple")){
+    console.log("I really like an Apple");
+}
+if (favorite_fruit.includes("Orange")){
+    console.log("I really like orange");
+}
+if (favorite_fruit.includes("Banana")){
+    console.log("I realy like Banana");
+}
