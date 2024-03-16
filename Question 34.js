78b13c3e3f0b3f625e7fa1c0b1471dbf877f74d1
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-17 00:54:15 +0500

    File of Question 34

---
+++ Question 34.js
@@ -0,0 +1,10 @@
+// creating a Array
+var pizza = ["chicken tika", "Malai cheese", "Fajita"];
+// using For -Loop
+for (var _i = 0, pizza_1 = pizza; _i < pizza_1.length; _i++) {
+    var onepizza = pizza_1[_i];
+    console.log("I like ".concat(onepizza, " pizza"));
+}
+//print a message outside of the For -Loop
+console.log("Pizza is love");
+console.log("I eat pizza");
---
+++ Question 34.ts
@@ -0,0 +1,10 @@
+// creating a Array
+let pizza =["chicken tika","Malai cheese","Fajita"]
+
+// using For -Loop
+for(let onepizza of pizza){
+    console.log(`I like ${onepizza} pizza`);
+}
+//print a message outside of the For -Loop
+console.log("Pizza is love");
+console.log("I eat pizza");
