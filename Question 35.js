8d5dcf9f9c4d40ec094f8f16d5dd84b664c3ae5f
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-17 00:54:59 +0500

    File of Question 35

---
+++ Question 35.js
@@ -0,0 +1,9 @@
+// creating a Array
+var Petanimals = ["Cat", "Dog", "Rabbit"];
+// using For -Loop
+for (var _i = 0, Petanimals_1 = Petanimals; _i < Petanimals_1.length; _i++) {
+    var onepet = Petanimals_1[_i];
+    console.log("A ".concat(onepet, " would make a great pet."));
+}
+// print a message outside of the For-Loop
+console.log("Any of theses animals would make a great pet! ");
---
+++ Question 35.ts
@@ -0,0 +1,9 @@
+// creating a Array
+let Petanimals =["Cat","Dog","Rabbit"]
+
+// using For -Loop
+for(let onepet of Petanimals){
+    console.log(`A ${onepet} would make a great pet.`)
+}
+// print a message outside of the For-Loop
+console.log("Any of theses animals would make a great pet! ");
