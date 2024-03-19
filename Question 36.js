9bca7df6061d1f0ace475bc8ddb2fd4ba154d137
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-20 02:38:11 +0500

    File of Question 36

---
+++ Question 36.js
@@ -0,0 +1,6 @@
+function make_shirt(size, printMessage) {
+    console.log("you selected ".concat(size, " size shirt with ").concat(printMessage, " prints on shirt"));
+}
+make_shirt("Large", "code with Typescript");
+make_shirt("Medium", "code with Typescript");
+make_shirt("small", "Huma");
---
+++ Question 36.ts
@@ -0,0 +1,7 @@
+
+function make_shirt (size:string,printMessage:string){
+console.log(`you selected ${size} size shirt with ${printMessage} prints on shirt`)
+}
+make_shirt("Large","code with Typescript");
+make_shirt("Medium","code with Typescript");
+make_shirt("small","Huma"); 
