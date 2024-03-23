cdd4abef338ead47056b5721e75383827de375f4
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-23 16:27:37 +0500

    File of Question 39

---
+++ Question 39.js
@@ -0,0 +1,7 @@
+// making a Function of city-country
+function city_country(city, country) {
+    return "".concat(city, ",").concat(country);
+}
+console.log(city_country("Karachi", "Pakistan"));
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Paris", "France"));
---
+++ Question 39.ts
@@ -0,0 +1,11 @@
+// making a Function of city-country
+function city_country(city:string,country:string){
+
+return `${city},${country}`;
+}
+
+console.log(city_country(`Karachi`,`Pakistan`));
+
+console.log(city_country(`Tokyo`,`Japan`));
+
+console.log(city_country(`Paris`,`France`));
