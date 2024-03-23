8b0d685853be8b716b0921c9d552a48f8bb5b108
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-03-23 16:29:07 +0500

    File of Question 41

---
+++ Question 41.js
@@ -0,0 +1,8 @@
+// making Array
+var magicians = ["Alice", "John", "Sam"];
+function show_magicians(magicians) {
+    magicians.forEach(function (magicians) {
+        console.log(magicians);
+    });
+}
+show_magicians(magicians);
---
+++ Question 41.ts
@@ -0,0 +1,16 @@
+// making Array
+ let magicians:string []=["Alice","John","Sam"];
+
+ function show_magicians(magicians:string[]){
+
+    magicians.forEach(magicians =>{
+    console.log(magicians) ;   
+    }
+    )
+
+ }
+show_magicians(magicians);
+
+
+    
+ 
